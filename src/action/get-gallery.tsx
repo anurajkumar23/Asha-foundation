03f9428ec5577e6d2a03e00969a87c02ceed52cf
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2024-03-31 22:30:07 +0530

    make gellery get as array[]

--- src/action/get-gallery.tsx
+++ src/action/get-gallery.tsx
@@ -2,7 +2,7 @@ import { Gallery } from "../../types";
 
 const URL=`${process.env.NEXT_PUBLIC_API_URL}/gallery`;
 
-const getGallery = async (): Promise<Gallery> => {
+const getGallery = async (): Promise<Gallery[]> => {
   const res = await fetch(`${URL}`,
   { cache: 'no-store' }
   );
